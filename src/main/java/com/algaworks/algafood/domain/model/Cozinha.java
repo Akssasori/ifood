ed838adb5c21cf0b20a7b5451586114e03b526c9
Akssasori <diniz3003@gmail.com>
2022-08-03 09:02:10 -0300

    aplication context

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,7 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
@@ -9,6 +10,7 @@ import java.util.Objects;
 public class Cozinha {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
